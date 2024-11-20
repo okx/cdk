a563026aa28f3a389130cdfbbe34255ad4307269
Vui-Chee <vuicheesiew@gmail.com>
2024-11-20 09:36:37 +0800

    Initial port of signer code from xlayer-node

---
+++ etherman/mock_etherman.go
@@ -0,0 +1,87 @@
+package etherman
+
+import (
+	"errors"
+	"fmt"
+	"math/big"
+
+	polygonzkevm "github.com/0xPolygon/cdk-contracts-tooling/contracts/banana/polygonvalidiumetrog"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+// BuildMockSequenceBatchesTxData builds a []bytes to be sent to the PoE SC method SequenceBatches.
+func (etherMan *Client) BuildMockSequenceBatchesTxData(sender common.Address,
+	validiumBatchData []polygonzkevm.PolygonValidiumEtrogValidiumBatchData,
+	maxSequenceTimestamp uint64,
+	l2Coinbase common.Address,
+	dataAvailabilityMessage []byte,
+	l1InfoTreeLeafCount uint32,
+	expectedInputHash string,
+) (to *common.Address, data []byte, err error) {
+	opts, err := etherMan.getAuthByAddress(sender)
+	if errors.Is(err, ErrNotFound) {
+		return nil, nil, fmt.Errorf("failed to build sequence batches, err: %w", ErrPrivateKeyNotFound)
+	}
+	opts.NoSend = true
+	// force nonce, gas limit and gas price to avoid querying it from the chain
+	opts.Nonce = big.NewInt(1)
+	opts.GasLimit = uint64(1)
+	opts.GasPrice = big.NewInt(1)
+
+	tx, err := etherMan.sequenceMockBatches(
+		opts,
+		validiumBatchData,
+		maxSequenceTimestamp,
+		l2Coinbase,
+		dataAvailabilityMessage,
+		l1InfoTreeLeafCount,
+		expectedInputHash,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return tx.To(), tx.Data(), nil
+}
+
+func (etherMan *Client) sequenceMockBatches(opts bind.TransactOpts,
+	validiumBatchData []polygonzkevm.PolygonValidiumEtrogValidiumBatchData,
+	maxSequenceTimestamp uint64,
+	l2Coinbase common.Address,
+	dataAvailabilityMessage []byte,
+	l1InfoTreeLeafCount uint32,
+	expectedInputHash string,
+) (*types.Transaction, error) {
+	var tx *types.Transaction
+	var err error
+
+	var finalInputHash = [32]byte{}
+	// Request will a hex string beginning with "0x...", so strip first 2 chars.
+	for i, bb := range common.Hex2Bytes(expectedInputHash[2:]) {
+		finalInputHash[i] = bb
+	}
+
+	tx, err = etherMan.ZkEVM.SequenceBatchesValidium(
+		&opts,
+		validiumBatchData,
+		l1InfoTreeLeafCount,
+		maxSequenceTimestamp,
+		finalInputHash,
+		l2Coinbase,
+		dataAvailabilityMessage,
+	)
+
+	if err != nil {
+		if parsedErr, ok := TryParseError(err); ok {
+			err = parsedErr
+		}
+		err = fmt.Errorf(
+			"error sequencing batches: %w, dataAvailabilityMessage: %s",
+			err, common.Bytes2Hex(dataAvailabilityMessage),
+		)
+	}
+
+	return tx, err
+}
---
+++ signer/service/type.go
@@ -0,0 +1,83 @@
+package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// Request is the request body
+type Request struct {
+	OperateType    int    `json:"operateType"`
+	OperateAddress string `json:"operateAddress"`
+	Symbol         int    `json:"symbol"`
+	ProjectSymbol  int    `json:"projectSymbol"`
+	RefOrderId     string `json:"refOrderId"`
+	OperateSymbol  int    `json:"operateSymbol"`
+	OperateAmount  int    `json:"operateAmount"`
+	SysFrom        int    `json:"sysFrom"`
+	OtherInfo      string `json:"otherInfo"`
+}
+
+func (args *Request) String() string {
+	b, err := json.Marshal(*args)
+	if err != nil {
+		return fmt.Sprintf("%+v", *args)
+	}
+	var out bytes.Buffer
+	err = json.Indent(&out, b, "", "    ")
+	if err != nil {
+		return fmt.Sprintf("%+v", *args)
+	}
+	return out.String()
+}
+
+// SeqData is the data for sequence operation
+type SeqData struct {
+	Batches                   []Batch        `json:"batches"`
+	MaxSequenceTimestamp      uint64         `json:"maxSequenceTimestamp"`
+	L2Coinbase                common.Address `json:"l2Coinbase"`
+	DataAvailabilityMessage   string         `json:"dataAvailabilityMessage"`
+	ContractAddress           common.Address `json:"contractAddress"`
+	GasLimit                  uint64         `json:"gasLimit"`
+	GasPrice                  string         `json:"gasPrice"`
+	Nonce                     uint64         `json:"nonce"`
+	L1InfoTreeLeafCount       uint32         `json:"l1InfoTreeLeafCount"`
+	ExpectedFinalAccInputHash string         `json:"expectedFinalAccInputHash"`
+}
+
+// Batch is the data for batch operation
+type Batch struct {
+	TransactionsHash     string `json:"transactionsHash"`
+	ForcedGlobalExitRoot string `json:"forcedGlobalExitRoot"`
+	ForcedTimestamp      uint64 `json:"forcedTimestamp"`
+	ForcedBlockHashL1    string `json:"forcedBlockHashL1"`
+}
+
+// AggData is the data for aggregate operation
+type AggData struct {
+	RollupId         uint64         `json:"rollupID"`
+	NewLocalExitRoot string         `json:"newLocalExitRoot"`
+	NewStateRoot     string         `json:"newStateRoot"`
+	FinalNewBatch    uint64         `json:"finalNewBatch"`
+	Proof            []string       `json:"proof"`
+	InitNumBatch     uint64         `json:"initNumBatch"`
+	PendingStateNum  int            `json:"pendingStateNum"`
+	ContractAddress  common.Address `json:"contractAddress"`
+	GasLimit         uint64         `json:"gasLimit"`
+	GasPrice         string         `json:"gasPrice"`
+	Nonce            uint64         `json:"nonce"`
+	Beneficiary      common.Address `json:"beneficiary"`
+}
+
+// Response is the response body
+type Response struct {
+	Code      int    `json:"code"`
+	Data      string `json:"data"`
+	DetailMsg string `json:"detailMsg"`
+	Msg       string `json:"msg"`
+	Status    int    `json:"status"`
+	Success   bool   `json:"success"`
+}
